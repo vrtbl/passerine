304b0ec9781d3ebe8bb585f440d371e1bd1a9a64
Isaac Clayton <slightknack@gmail.com>
2022-03-19 08:15:23 +0100

    Wrote some property-based tests for Passerine, fixed a few small errors in reader

--- src/compiler/read.rs
+++ src/compiler/read.rs
@@ -6,7 +6,7 @@ pub struct Reader {
     tokens: Tokens,
     index: usize,
     // stack of nested groupings
-    opening: Vec<(usize, Spanned<Delim>)>,
+    opening: Vec<Spanned<Delim>>,
 }
 
 impl Reader {
@@ -20,14 +20,12 @@ impl Reader {
         let result = reader.block();
 
         // if there are still unclosed delimiters on the opening stack
-        if reader.opening.len() > 0 {
-            let (_index, still_opened) = reader.opening.last().unwrap();
-            let error = Syntax::error(
-                &format!(
-                    "Unclosed {}", still_opened.item,
-                ), &still_opened.span);
-
-            Err(error)
+        if !reader.opening.is_empty() {
+            let still_opened = reader.opening.last().unwrap();
+            Err(Syntax::error(
+                &format!("Unclosed opening {}", still_opened.item,),
+                &still_opened.span,
+            ))
         } else {
             result
         }
@@ -90,6 +88,7 @@ impl Reader {
     fn block(&mut self) -> Result<TokenTree, Syntax> {
         let mut lines = vec![];
         let mut line = vec![];
+        let mut after_sep = false;
         let mut after_op = false;
 
         while let Some(token) = self.next_token() {
@@ -103,16 +102,14 @@ impl Reader {
                     break;
                 },
                 Token::Sep => {
-                    if !after_op && line.len() > 0 {
-                        lines.push(line);
-                        line = vec![];
-                    }
+                    after_sep = true;
                     continue;
                 },
 
                 Token::Op(op) => {
                     let spanned = Spanned::new(TokenTree::Op(op), span);
                     line.push(spanned);
+                    after_sep = false;
                     after_op = true;
                     continue;
                 },
@@ -121,6 +118,12 @@ impl Reader {
                 other => Self::trivial(other).unwrap(),
             };
 
+            if after_sep && !after_op && !line.is_empty() {
+                lines.push(line);
+                line = vec![];
+            }
+
+            after_sep = false;
             after_op = false;
             let combined = Spanned::new(item, span);
             line.push(combined);
@@ -134,7 +137,7 @@ impl Reader {
         &mut self,
         delim: Spanned<Delim>,
     ) -> Result<TokenTree, Syntax> {
-        self.opening.push((self.index, delim.clone()));
+        self.opening.push(delim.clone());
 
         let tree = match delim.item {
             Delim::Curly => self.block()?,
@@ -149,7 +152,7 @@ impl Reader {
         &mut self,
         closing_delim: Spanned<Delim>,
     ) -> Result<(), Syntax> {
-        let (index, opening_delim) = self.opening.pop().ok_or_else(|| {
+        let opening_delim = self.opening.pop().ok_or_else(|| {
             Syntax::error(
                 &format!("Unexpected closing {}", closing_delim.item),
                 &closing_delim.span,
@@ -176,84 +179,121 @@ impl Reader {
 
 #[cfg(test)]
 mod test {
+    use std::fmt::Write;
+
     use super::*;
     use crate::common::source::Source;
     use crate::compiler::lex::Lexer;
 
     use proptest::prelude::*;
 
+    /// Generates a source file from some tokens.
+    /// Replaces each token with a minimal representative token.
+    /// for example, all delimiters become curly brackets,
+    /// all identifiers become `x`, and so on.
+    fn generate_minimal_source(tokens: &[Token]) -> String {
+        let mut buffer = String::new();
+        for token in tokens {
+            let new = match token {
+                Token::Open(_) => "{",
+                Token::Close(_) => "}",
+                Token::Sep => "\n",
+                Token::Iden(_) => " x ",
+                Token::Label(_) => " X ",
+                Token::Op(_) => " + ",
+                Token::Lit(_) => " 2 ",
+            };
+            buffer.write_str(new).unwrap();
+        }
+        buffer
+    }
+
+    /// Checks if there are a matching number of opening and closing delims.
+    /// Doesn't care if delims are of different types
+    /// To be used with `generate_minimal_source`.
+    fn check_if_balanced(tokens: &[Token]) -> bool {
+        let mut delims = 0;
+
+        for token in tokens {
+            match token {
+                Token::Open(_) => delims += 1,
+                Token::Close(_) => delims -= 1,
+                _ => continue,
+            };
+
+            if delims < 0 {
+                return false;
+            }
+        }
+
+        delims == 0
+    }
+
     proptest! {
         #[test]
-        fn random_tokens(tokens: Token) {
-            dbg!(tokens);
+        fn check_balance(tokens: Vec<Token>) {
+            let balanced = check_if_balanced(&tokens);
+            let source = generate_minimal_source(&tokens);
+            dbg!(&source);
+            let tokens = Lexer::lex(Source::source(&source)).unwrap();
+            let token_tree = Reader::read(tokens);
+
+            if balanced {
+                println!("balanced");
+                assert!(token_tree.is_ok())
+            } else {
+                println!("unbalanced");
+                assert!(token_tree.is_err())
+            }
+        }
+
+        #[test]
+        fn multiline_ops(splits: Vec<bool>) {
+            // generate a random valid buffer
+            let mut buffer = "2".to_string();
+            for (index, split) in splits.iter().enumerate() {
+                let sep = if *split { '\n' } else { ' ' };
+                let token = if index % 2 == 0 { '+' } else { '2' };
+                buffer.write_char(sep).unwrap();
+                buffer.write_char(token).unwrap();
+            }
+            if splits.len() % 2 == 1 {
+                buffer.write_char('2').unwrap();
+            }
+
+            // check that the buffer is valid
+            dbg!(&buffer);
+            let tokens = Lexer::lex(Source::source(&buffer)).unwrap();
+            let token_tree = Reader::read(tokens);
+            dbg!(&token_tree);
+            assert!(token_tree.is_ok());
+            if let TokenTree::Block(block) = token_tree.unwrap() {
+                assert_eq!(block.len(), 1);
+            }
         }
     }
 
     #[test]
-    fn example_test() {
+    fn form_with_group() {
         let source = Source::source("print (1 + 2)");
         let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
-        let token_tree = Reader::read(tokens).unwrap();
-        dbg!(token_tree);
+        let token_tree = Reader::read(tokens);
+        assert!(token_tree.is_ok())
     }
-
-
-    // Hey, if you're here, you're here for some compiler hacking.
-    // So I just wrote this code, but I haven't really tested this.
-    // Your quest, should you choose to embark on it, is to write some tests,
-    // and fix the places this pass falls short.
-
-    // This pass takes a flat list of Tokens (found in src/contruct/token)
-    // and produces a TokenTree, essentially a list of tokens where groups of delimiters are nested.
-
-    // For example, the code:
-    // print (1 + 2)
-    // Produces the Tokens:
-    // Identifier print
-    // Open (
-    // Literal 1
-    // Operator +
-    // Literal 1
-    // Close )
-
-    // Note how the parenthesis aren't matched yet.
-    // This pass keeps track of parenthesis and other delimiters,
-    // and builds nested lists of tokens:
-
-    // For example, the code:
-    // print (1 + 2)
-    // Produces the TokenTree:
-    // Identifier print
-    // Form (
-    //     Literal 1
-    //     Operator +
-    //     Literal 1
-    // )
-
-    // Note how the parenthesis have been replaced with a single form
-    // containing another list of tokens.
-
-    // Look at example test, and see if you can find any input that "breaks the parser"
-    // You can also try writing a property based test to help find unit tests.
-
-    // Good luck! let me know if you have any questions.
 
     #[test]
     fn list() {
         let source = Source::source("[a, b, c]");
         let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
-        let token_tree = Reader::read(tokens).unwrap();
-        dbg!(token_tree);
+        let token_tree = Reader::read(tokens);
+        assert!(token_tree.is_ok())
     }
 
-    // Should return a syntax error - mismatched delimiters
+    /// Should return a syntax error - mismatched delimiters
     #[test]
     fn unbalanced_delims() {
         let source = Source::source("([)]");
         let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
         let token_tree = Reader::read(tokens);
         assert!(token_tree.is_err());
     }
@@ -262,47 +302,50 @@ mod test {
     fn balanced_delims() {
         let source = Source::source("[(),[],{()[]}]{([][]){}}");
         let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
-        let token_tree = Reader::read(tokens).unwrap();
-        dbg!(token_tree);
+        let token_tree = Reader::read(tokens);
+        assert!(token_tree.is_ok())
     }
 
     #[test]
     fn multiline_operators() {
         let source = Source::source("1 ++\n2 --\n 3");
         let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
-        let token_tree = Reader::read(tokens).unwrap();
-        dbg!(token_tree);
+        let token_tree = Reader::read(tokens);
+        assert!(token_tree.is_ok())
     }
 
     #[test]
-    fn unclosed_paren() {
+    fn unclosed_opening_paren() {
         let source = Source::source("(");
         let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
         let token_tree = Reader::read(tokens);
         assert!(token_tree.is_err());
-        dbg!(token_tree.err().unwrap());
     }
 
-    // honestly, I'm not sure how this should be parsed, but I'm adding it anyway
+    #[test]
+    fn unclosed_closing_paren() {
+        let source = Source::source(")");
+        let tokens = Lexer::lex(source).unwrap();
+        let token_tree = Reader::read(tokens);
+        assert!(token_tree.is_err());
+    }
+
     #[test]
     fn multiline_block() {
-        let source = Source::source("\n2 \n+ \n2\n");
-        let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
-        let token_tree = Reader::read(tokens).unwrap();
-        dbg!(token_tree);
+        let tokens = Lexer::lex(Source::source("2\n+2")).unwrap();
+        let token_tree = Reader::read(tokens);
+        dbg!(&token_tree);
+        assert!(token_tree.is_ok());
+        if let TokenTree::Block(block) = token_tree.unwrap() {
+            assert_eq!(block.len(), 1);
+        }
     }
 
-    // honestly, I'm not sure how this should be parsed, but I'm adding it anyway
     #[test]
     fn multiline_form() {
         let source = Source::source("(\n2 \n+ 2\n)");
         let tokens = Lexer::lex(source).unwrap();
-        dbg!(&tokens);
-        let token_tree = Reader::read(tokens).unwrap();
-        dbg!(token_tree);
+        let token_tree = Reader::read(tokens);
+        assert!(token_tree.is_ok())
     }
 }
