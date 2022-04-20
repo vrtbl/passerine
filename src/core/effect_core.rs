67ca0569d827c0f14312d9ce0976da3e17ee32d9
Isaac Clayton <slightknack@gmail.com>
2022-04-20 08:07:54 +0200

    working on effect core

--- src/core/effect_core.rs
+++ src/core/effect_core.rs
@@ -1,7 +1,24 @@
-pub enum EffectCore {
+pub enum Core {
     Fatal,
-    WriteStdOut(String),
-    WriteStdErr(String),
-    ReadStdIn(String),
+    WriteOut(String),
+    WriteErr(String),
+    ReadIn(String),
     ToString(Data),
+}
+
+pub trait Effect {
+    fn from_data(data: Data) -> Result<Self, String>;
+}
+
+pub struct EffectSet<T> {
+    effects: HashMap,
+}
+
+pub fn main() {
+    effect_set! {
+        enum E {
+            "std.Core" => Core,
+            "std.http.IO" => Http,
+        }
+    };
 }
